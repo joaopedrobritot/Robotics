3a992aa6c68338088a940168e2d4ef091509f5b7
João Pedro Brito Tomé <47987224+joaopedrobritot@users.noreply.github.com>
2022-07-18 20:22:23 -0300

    Add files via upload

--- Projeto_Robo_UR5/Projeto_robo.py
+++ Projeto_Robo_UR5/Projeto_robo.py
@@ -1,7 +1,14 @@
+# Projeto de Robô para disciplina Robótica
+# Professor Glauber Rodrigues Leite
+# Dupla:
+#       João Pedro Tomé
+#       Igor Hutson
+
 import roboticstoolbox as rtb
 import spatialmath as sm
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 from numpy import sin, cos, pi
 from roboticstoolbox import *
 import sim
@@ -37,18 +44,24 @@ if clientID != -1:
     sim.simxAddStatusbarMessage(clientID,'Connection working...',sim.simx_opmode_oneshot_wait)
     time.sleep(0.05)
     
-    err, joint1 = sim.simxGetObjectHandle(clientID, '/UR5/joint', sim.simx_opmode_oneshot_wait)
-    err, joint2 = sim.simxGetObjectHandle(clientID, '/UR5/link/joint', sim.simx_opmode_oneshot_wait)
-    err, joint3 = sim.simxGetObjectHandle(clientID, '/UR5/link/joint/link/joint', sim.simx_opmode_oneshot_wait)
-    err, joint4 = sim.simxGetObjectHandle(clientID, '/UR5/link/joint/link/joint/link/joint', sim.simx_opmode_oneshot_wait)
-    err, joint5 = sim.simxGetObjectHandle(clientID, '/UR5/link/joint/link/joint/link/joint/link/joint', sim.simx_opmode_oneshot_wait)
-    err, joint6 = sim.simxGetObjectHandle(clientID, '/UR5/link/joint/link/joint/link/joint/link/joint/link/joint', sim.simx_opmode_oneshot_wait)
-    err, pad = sim.simxGetObjectHandle(clientID, '/UR5/link7_visible', sim.simx_opmode_oneshot_wait)
-    err, dummy = sim.simxGetObjectHandle(clientID, '/Dummy', sim.simx_opmode_oneshot_wait)
-    
+    _, joint1 = sim.simxGetObjectHandle(clientID, '/UR5/joint', sim.simx_opmode_oneshot_wait)
+    _, joint2 = sim.simxGetObjectHandle(clientID, '/UR5/link/joint', sim.simx_opmode_oneshot_wait)
+    _, joint3 = sim.simxGetObjectHandle(clientID, '/UR5/link/joint/link/joint', sim.simx_opmode_oneshot_wait)
+    _, joint4 = sim.simxGetObjectHandle(clientID, '/UR5/link/joint/link/joint/link/joint', sim.simx_opmode_oneshot_wait)
+    _, joint5 = sim.simxGetObjectHandle(clientID, '/UR5/link/joint/link/joint/link/joint/link/joint', sim.simx_opmode_oneshot_wait)
+    _, joint6 = sim.simxGetObjectHandle(clientID, '/UR5/link/joint/link/joint/link/joint/link/joint/link/joint', sim.simx_opmode_oneshot_wait)
+    _, atuador = sim.simxGetObjectHandle(clientID, '/UR5/link7_visible', sim.simx_opmode_oneshot_wait)
+    _, dummy = sim.simxGetObjectHandle(clientID, '/Dummy', sim.simx_opmode_oneshot_wait)
+
+    val = sim.simxGetJointPosition(clientID, joint1, sim.simx_opmode_streaming)
+    val = sim.simxGetJointPosition(clientID, joint2, sim.simx_opmode_streaming)
+    val = sim.simxGetJointPosition(clientID, joint3, sim.simx_opmode_streaming)
+    val = sim.simxGetJointPosition(clientID, joint4, sim.simx_opmode_streaming)
+    val = sim.simxGetJointPosition(clientID, joint5, sim.simx_opmode_streaming)
+    val = sim.simxGetJointPosition(clientID, joint6, sim.simx_opmode_streaming)
     err, d = sim.simxGetObjectPosition(clientID, dummy, -1, sim.simx_opmode_streaming)
-    err, X = sim.simxGetObjectPosition(clientID, pad, -1, sim.simx_opmode_streaming)
-    
+    err, X = sim.simxGetObjectPosition(clientID, atuador, -1, sim.simx_opmode_streaming)
+
     sim.simxSetJointTargetPosition(clientID, joint1, 0, sim.simx_opmode_oneshot)
     time.sleep(0.05)
     sim.simxSetJointTargetPosition(clientID, joint2, 0, sim.simx_opmode_oneshot)
@@ -64,71 +77,64 @@ if clientID != -1:
     time.sleep(1)
     
     delta_t = 0.1
-    error1 = []
-    error2 = []
-    error3 = []
-    error4 = []
-    error5 = []
-    error6 = []
-     
-    while True:
-        err, q1 = sim.simxGetJointPosition(clientID, joint1, sim.simx_opmode_buffer)
-        err, q2 = sim.simxGetJointPosition(clientID, joint2, sim.simx_opmode_buffer)
-        err, q3 = sim.simxGetJointPosition(clientID, joint3, sim.simx_opmode_buffer)
-        err, q4 = sim.simxGetJointPosition(clientID, joint4, sim.simx_opmode_buffer)
-        err, q5 = sim.simxGetJointPosition(clientID, joint5, sim.simx_opmode_buffer)
-        err, q6 = sim.simxGetJointPosition(clientID, joint6, sim.simx_opmode_buffer)
+    e1, e2, e3, e4, e5, e6 = ([] for i in range(6))
+    
+    j = 100
+    while j > 0:
+        _, q1 = sim.simxGetJointPosition(clientID, joint1, sim.simx_opmode_buffer)
+        _, q2 = sim.simxGetJointPosition(clientID, joint2, sim.simx_opmode_buffer)
+        _, q3 = sim.simxGetJointPosition(clientID, joint3, sim.simx_opmode_buffer)
+        _, q4 = sim.simxGetJointPosition(clientID, joint4, sim.simx_opmode_buffer)
+        _, q5 = sim.simxGetJointPosition(clientID, joint5, sim.simx_opmode_buffer)
+        _, q6 = sim.simxGetJointPosition(clientID, joint6, sim.simx_opmode_buffer)
         time.sleep(0.05)
         q = [q1, q2, q3, q4, q5, q6]
                 
-        err, d = sim.simxGetObjectPosition(clientID, dummy, -1, sim.simx_opmode_buffer)    
-        err, X = sim.simxGetObjectPosition(clientID, pad, sim.sim_handle_parent, sim.simx_opmode_buffer)
+        _, d = sim.simxGetObjectPosition(clientID, dummy, -1, sim.simx_opmode_buffer)    
+        _, X = sim.simxGetObjectPosition(clientID, atuador, sim.sim_handle_parent, sim.simx_opmode_buffer)
         time.sleep(0.05)
         
-        pad_pose = robot.fkine(q)
-        dummy_pose = sm.SE3(d[0], d[1], d[2])
-                
-        v, arrived = rtb.p_servo(pad_pose, dummy_pose, 1)
-        
-        J = robot.jacobe(q)
-        J_inv = np.linalg.pinv(J)
-        
-        q_dot = J_inv @ v                
+        atuador_pose = robot.fkine(q)
+        dum_pose = sm.SE3(d[0], d[1], d[2])   
+        v, arrived = rtb.p_servo(atuador_pose, dum_pose, 1)
+        Jacob = robot.jacobe(q)
+        Jacob_inv = np.linalg.pinv(Jacob)
+        q_dot = Jacob_inv @ v                
         q += q_dot*(delta_t)
+
+        error = np.linalg.norm(np.array(d) - np.array(X))
+        if error < 0.05:
+            break
         
-        error1.append(q_dot[0])
-        error2.append(q_dot[1])
-        error3.append(q_dot[2])
-        error4.append(q_dot[3])
-        error5.append(q_dot[4])
-        error6.append(q_dot[5])
-        
-        np.save('error_plot_ur5.npy', np.array([error1, error2, error3, error4, error5, error6]))
-        
-        sim.simxSetJointTargetPosition(clientID, joint1, q[0], sim.simx_opmode_oneshot)
-        time.sleep(0.05)
-        
-        sim.simxSetJointTargetPosition(clientID, joint2, q[1], sim.simx_opmode_oneshot)
-        time.sleep(0.05)
-        
-        sim.simxSetJointTargetPosition(clientID, joint3, q[2], sim.simx_opmode_oneshot)
-        time.sleep(0.05)
-        
-        sim.simxSetJointTargetPosition(clientID, joint4, q[3], sim.simx_opmode_oneshot)
-        time.sleep(0.05)
-        
-        sim.simxSetJointTargetPosition(clientID, joint5, q[4], sim.simx_opmode_oneshot)
-        time.sleep(0.05)
-        
-        sim.simxSetJointTargetPosition(clientID, joint6, q[5], sim.simx_opmode_oneshot)
-        time.sleep(0.05)
-        
-        time.sleep(0.05)
-        
+        e1.append(q_dot[0])
+        e2.append(q_dot[1])
+        e3.append(q_dot[2])
+        e4.append(q_dot[3])
+        e5.append(q_dot[4])
+        e6.append(q_dot[5])
+
+        joints = [joint1, joint2, joint3, joint4, joint5, joint6]
+
+        for i in range(0, 6):
+            sim.simxSetJointTargetPosition(clientID, joints[i], q[i], sim.simx_opmode_oneshot)
+            time.sleep(0.05)
+
+        j -= 1
+
+    plt.plot(e1, color='blue', label='Joint 1', lw=2)
+    plt.plot(e2, color='red', label='Joint 2', lw=2)
+    plt.plot(e3, color='green', label='Joint 3', lw=2)
+    plt.plot(e4, color='yellow', label='Joint 4', lw=2)
+    plt.plot(e5, color='magenta', label='Joint 5', lw=2)
+    plt.plot(e6, color='turquoise', label='Joint 6', lw=2)
+    plt.xlim([0,100])
+    plt.ylim([-60, 60])
+    plt.title("Erro para cada junta")
+    plt.legend()
+    plt.savefig('D:\joaop\Desktop\Robotica\Projeto_robo\ErroJuntas.png')
+    plt.show()
 
     sim.simxPauseSimulation(clientID,sim.simx_opmode_oneshot_wait)
-
-    sim.simxAddStatusbarMessage(clientID, 'Program paused', sim.simx_opmode_blocking )
     sim.simxFinish(clientID)
 else:
     print ('Failed connecting to remote API server')
